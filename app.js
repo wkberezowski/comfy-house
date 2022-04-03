b2bbf9b89797a8a4857cf4eee2a326dce5304d0b
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-04-04 01:03:10 +0200

    set up the headless CMS for the application

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: 'ofkvjh4aysdk',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: 'p3vmdnj3XWcZHkcUsD7JsHs1kaWsxHHuACrGKcLMF0E',
+});
+
 // variables
 
 const cartBtn = document.querySelector('.cart-btn');
@@ -19,10 +26,13 @@ let buttonsDOM = [];
 class Products {
   async getProducts() {
     try {
+      let contentful = await client.getEntries({
+        content_type: 'comfyHouse',
+      });
       let result = await fetch('products.json');
       let data = await result.json();
 
-      let products = data.items;
+      let products = contentful.items;
       products = products.map((item) => {
         const { title, price } = item.fields;
         const { id } = item.sys;
